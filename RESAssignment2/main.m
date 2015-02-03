75be6445e84f5c50c441e01d58c2bac451e9b83d
Robert Slatkin <robertslatkin@Roberts-MacBook-Pro.local>
2015-02-03 14:00:46 -0500

    Initial Commit

---
+++ RESAssignment2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RESAssignment2
+//
+//  Created by Robert Slatkin on 2/3/15.
+//  Copyright (c) 2015 Robert Slatkin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
